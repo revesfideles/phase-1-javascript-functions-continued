97604fefba46428e2d29c4f73e0976504234c7f0
revesfideles <revesfideles@gmail.com>
2024-02-21 16:20:45 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(rollerSkate = "roller-skate") {
+    const message = `This Saturday, I want to ${rollerSkate}!`;
+    console.log(message);
+    return(message);
+    }
+
+function mondayWork(goToTheOffice = "go to the office") {
+    const mondayMessage = `This Monday, I will ${goToTheOffice}.`;
+    console.log(mondayMessage);
+    return(mondayMessage);
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are !!!a dedicated programmer!!!"
